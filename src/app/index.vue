23305b7e06637c12150bb559720ad11535e01d42
Ne Vlad Savelyev <mixalpalf@gmail.com>
2024-09-18 11:08:52 +0300

    Set up google console

--- src/app/index.vue
+++ src/app/index.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { EditMode } from '~/shared/types/editmode';
 import { Header } from '~/widgets/Header';
+
+useHead({
+  meta: [
+    { name: 'google-site-verification', content: 'pi0rv86VBqXV4bksh6OcmwoG4XOsmyqQ7SPIMrBbHGs' },
+  ],
+})
 </script>
 
 <template>
